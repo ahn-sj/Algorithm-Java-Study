8151b92b7510c6d94d23cc7a4acf1d31ad396ce3
ahn-sj <jaesa5221@gmail.com>
2022-12-29 19:56:39 +0900

    [BOJ] NO10828. 스택

---
+++ 성재/jae-workspace/src/main/java/study/jaeworkspace/baekjoon/NO10828.java
@@ -0,0 +1,70 @@
+package study.jaeworkspace.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 스택
+ */
+public class NO10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+        int loopCount = Integer.parseInt(scan.readLine());
+
+        CustomStack stack = new CustomStack();
+
+        for (int i = 0; i < loopCount; i++) {
+            String command = scan.readLine();
+            switch (command) {
+                case "pop":
+                    System.out.println(stack.pop());
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    System.out.println(stack.empty());
+                    break;
+                case "top":
+                    System.out.println(stack.top());
+                    break;
+                default: // push N
+                    stack.push(Integer.parseInt(command.split(" ")[1]));
+                    break;
+            }
+        }
+    }
+
+    public static class CustomStack {
+        int[] box = new int[10001];
+        int size = 0;
+
+        public void push(int element) {
+            box[size] = element;
+            size++;
+        }
+
+        public int pop() {
+            if(size == 0) {
+                return -1;
+            }
+            int temp = box[size - 1];
+            box[size - 1] = 0;
+            size--;
+            return temp;
+        }
+
+        public int size() {
+            return size;
+        }
+
+        public int empty() {
+            return size <= 0 ? 1 : 0;
+        }
+
+        public int top() {
+            return size <= 0 ? -1 : box[size - 1];
+        }
+    }
+}
