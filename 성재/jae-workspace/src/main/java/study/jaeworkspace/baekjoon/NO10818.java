f9349afb78a5ebf31c0bcd8cc552312512d03fcc
ahn-sj <jaesa5221@gmail.com>
2022-12-29 18:18:36 +0900

    [BOJ] NO10818. 최소, 최대

---
+++ 성재/jae-workspace/src/main/java/study/jaeworkspace/baekjoon/NO10818.java
@@ -0,0 +1,21 @@
+package study.jaeworkspace.baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * 최소, 최대
+ */
+public class NO10818 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] given = new int[scan.nextInt()];
+
+        for (int i = 0; i < given.length; i++) {
+            given[i] = scan.nextInt();
+        }
+        Arrays.sort(given);
+
+        System.out.println(given[0] + " " + given[given.length - 1]);
+    }
+}
