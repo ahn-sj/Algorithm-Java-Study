2dfef2c4f2a7694a5b1c84d0d0690d8370b8dabc
ahn-sj <jaesa5221@gmail.com>
2022-12-29 16:59:40 +0900

    Init Settings Jae Workspace

---
+++ 성재/jae-workspace/src/main/java/study/jaeworkspace/JaeWorkspaceApplication.java
@@ -0,0 +1,13 @@
+package study.jaeworkspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaeWorkspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaeWorkspaceApplication.class, args);
+	}
+
+}
