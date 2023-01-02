97fcab2f19f0cf4b7c48f537d23ad167bb70c2c4
ahn-sj <jaesa5221@gmail.com>
2023-01-02 19:42:10 +0900

    [BOJ] Queue Test

--- 성재/jae-workspace/src/main/java/study/jaeworkspace/baekjoon/NO10845.java
+++ 성재/jae-workspace/src/main/java/study/jaeworkspace/baekjoon/NO10845.java
@@ -1,30 +1,76 @@
 package study.jaeworkspace.baekjoon;
 
+import java.util.Stack;
+
 /**
  * 큐
  */
 public class NO10845 {
     public static void main(String[] args) {
 
+        CustomQueue queue = new CustomQueue();
+
+        queue.push(1);
+        queue.push(2);
+        queue.push(3);
+
+        System.out.println(queue.pop());
+        System.out.println(queue.pop());
+
+        System.out.println(queue.empty());
+
+        System.out.println(queue.front());
+        System.out.println(queue.back());
     }
 
     public static class CustomQueue {
-        int[] queueFiller = new int[10001];
-        int[] queueHandler = new int[10001];
-        int size = 0;
+        Stack<Integer> newStack = new Stack();
+        Stack<Integer> oldStack = new Stack();
 
         public void push(int element) {
-            queueFiller[size] = element;
-            size++;
+            newStack.push(element);
         }
 
         public int pop() {
-            return 0;
+            fillOldStack();
+            return oldStack.pop();
         }
 
         public int size() {
-            return size;
+            fillOldStack();
+            return oldStack.size();
         }
 
+        public int empty() {
+            fillOldStack();
+            return oldStack.empty() == true ? 1 : 0;
+        }
+
+        public int front() {
+            fillOldStack();
+
+            if(oldStack.empty()) return -1;
+
+            return oldStack.firstElement();
+        }
+
+        public int back() {
+            fillOldStack();
+
+            if(oldStack.empty()) return -1;
+            return oldStack.lastElement();
+        }
+
+
+        private void fillOldStack() {
+            while (hasElement()) {
+                Integer pop = newStack.pop();
+                oldStack.push(pop);
+            }
+        }
+
+        private boolean hasElement() {
+            return !newStack.empty();
+        }
     }
 }
