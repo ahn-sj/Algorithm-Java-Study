5494182bc340ecc81ad66fcbc37147714fc04a19
ahn-sj <jaesa5221@gmail.com>
2023-01-08 21:09:48 +0900

    [BOJ] NO2579. 계단오르기 - 2

--- 성재/jae-workspace/src/main/java/study/jaeworkspace/baekjoon/w03/NO2579.java
+++ 성재/jae-workspace/src/main/java/study/jaeworkspace/baekjoon/w03/NO2579.java
@@ -1,6 +1,7 @@
 package study.jaeworkspace.baekjoon.w03;
 
 import java.io.*;
+import java.util.Arrays;
 
 /**
  * 계단 오르기
@@ -12,10 +13,24 @@ public class NO2579 {
 
         int step = Integer.parseInt(br.readLine());
         int[] stairs = new int[step + 1];
+        int[] scores = new int[step + 1];
 
+        for (int i = 1; i <= step; i++) {
+            scores[i] = Integer.parseInt(br.readLine());
+        }
 
+        stairs[1] = scores[1];
+        stairs[2] = scores[2];
 
+        for (int i = 3; i < scores.length; i++) {
+            stairs[i] = Math.max(scores[i - 3] + scores[i - 1], scores[i - 2]) + scores[i];
+        }
+        System.out.println(Arrays.toString(stairs));
+        System.out.println(Arrays.toString(scores));
+        bw.write(stairs[step] + "");
+        bw.flush();
 
-
+        bw.close();
+        br.close();
     }
 }
