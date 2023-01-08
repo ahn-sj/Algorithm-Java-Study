99e7249a47a5c84656a51fb6ba34fc1cadf683e5
ahn-sj <jaesa5221@gmail.com>
2023-01-08 20:24:49 +0900

    [BOJ] NO2193. 이친수

---
+++ 성재/jae-workspace/src/main/java/study/jaeworkspace/baekjoon/w03/NO2193.java
@@ -0,0 +1,25 @@
+package study.jaeworkspace.baekjoon.w03;
+
+import java.io.*;
+
+public class NO2193 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int number = Integer.parseInt(br.readLine());
+
+        long[] pinaryNumbers = new long[91];
+
+        pinaryNumbers[2] = pinaryNumbers[1] = 1;
+
+        for (int i = 3; i <= number; i++) {
+            pinaryNumbers[i] = pinaryNumbers[i - 1] + pinaryNumbers[i - 2];
+        }
+        bw.write(pinaryNumbers[number] + "");
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
